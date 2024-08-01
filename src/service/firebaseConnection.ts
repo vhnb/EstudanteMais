b4d63368a46f80ed4a4e23c57a6eb8eb168104da
Victor Henrique. <123006884+vhnb@users.noreply.github.com>
2024-08-01 04:13:35 -0300

    Update firebaseConnection.ts

--- src/service/firebaseConnection.ts
+++ src/service/firebaseConnection.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDfT-BYar0G6J-MlpuErqf34rjSvqxQTv8",
-    authDomain: "estudanteplus-301c9.firebaseapp.com",
-    projectId: "estudanteplus-301c9",
-    storageBucket: "estudanteplus-301c9.appspot.com",
-    messagingSenderId: "805131025821",
-    appId: "1:805131025821:web:78e5b7c3cdcd7aad520374"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
